3aada7c48375916d46fbee7426f58776ad79aa97
S4L4H77 <salahamleh45@gmail.com>
2026-07-18 16:33:15 +0300

    fix some problems

--- lib/Components/Container Box.dart
+++ lib/Components/Container Box.dart
@@ -1,17 +1,13 @@
-
-
-
-
 import 'package:flutter/material.dart';
 
 class Box extends StatelessWidget {
-  String? Title;
-  Color? Box_color;
-  Color? Title_color;
-  double? FontSize;
+  final String? Title;
+  final Color? Box_color;
+  final Color? Title_color;
+  final double? FontSize;
 
-  Box({super.key, this.Box_color,this.Title,this.Title_color,this.FontSize});
-
+  const Box(
+      {super.key, this.Box_color, this.Title, this.Title_color, this.FontSize});
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +16,15 @@ class Box extends StatelessWidget {
       width: 227,
       height: 63,
       decoration: BoxDecoration(
-
           color: Box_color,
-          borderRadius: const BorderRadius.all(Radius.circular(30))
-
-      ),
+          borderRadius: const BorderRadius.all(Radius.circular(30))),
       child: Center(
         child: Text("$Title",
-            style: TextStyle(fontSize:FontSize,color: Title_color,fontWeight: FontWeight.w500,fontFamily: "Lobster")),
+            style: TextStyle(
+                fontSize: FontSize,
+                color: Title_color,
+                fontWeight: FontWeight.w500,
+                fontFamily: "Lobster")),
       ),
     );
   }
--- lib/Screens/Menu/MenuCoffeeScreen.dart
+++ lib/Screens/Menu/MenuCoffeeScreen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:coffeapp/Screens/OrderScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -13,224 +11,251 @@ class MenuCoffeeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff98694F),
-
-      body:  SizedBox(
+      body: SizedBox(
         width: double.infinity,
         child: Stack(
-
           children: [
-            Positioned(bottom: 0,right: 0,left: 0,child: SizedBox(width: double.infinity,child: Image.asset("assets/images/images11.png",fit: BoxFit.fitWidth,))),
-
-            Positioned(left: 30,top: 58,child: InkWell(onTap: (){
-              Navigator.pushNamed(context, "/MenuScreen");
-            } ,child: Image.asset("assets/images/Vector.png"))),
-
-            Positioned(right: 23,top: 53,child: InkWell(onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>const OrderScreen(),),);
-              
-            },child: Image.asset("assets/images/Order - icon.png")),),
-
-            const Positioned(top: 37,right: 15,child: SizedBox(
-              width: 24,
-              child: FloatingActionButton(
-                backgroundColor:Color(0xffE9CBA7) ,
-                onPressed: null,
-                child: Text("3",style: TextStyle(color: Color(0xff000000),fontWeight: FontWeight.bold ),),
-
-              ),
-            )),
-
-
-            const Positioned(top: 95,child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 144.0),
-              child: Text("Menu",style: TextStyle(color: kMainColor,fontSize: 48,fontFamily: "Lobster"),),
-            )),
-
-
-
-
-            Positioned(top:190,child: Column(
-              children: [
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: SizedBox(
+                    width: double.infinity,
+                    child: Image.asset(
+                      "assets/images/images11.png",
+                      fit: BoxFit.fitWidth,
+                    ))),
+            Positioned(
+                left: 30,
+                top: 58,
+                child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, "/MenuScreen");
+                    },
+                    child: Image.asset("assets/images/Vector.png"))),
+            Positioned(
+              right: 23,
+              top: 53,
+              child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const OrderScreen(),
                       ),
-
+                    );
+                  },
+                  child: Image.asset("assets/images/Order - icon.png")),
+            ),
+            const Positioned(
+                top: 37,
+                right: 15,
+                child: SizedBox(
+                  width: 24,
+                  child: FloatingActionButton(
+                    backgroundColor: Color(0xffE9CBA7),
+                    onPressed: null,
+                    child: Text(
+                      "3",
+                      style: TextStyle(
+                          color: Color(0xff000000),
+                          fontWeight: FontWeight.bold),
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
                   ),
-
-
-
-                ),
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+                )),
+            const Positioned(
+                top: 95,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 144.0),
+                  child: Text(
+                    "Menu",
+                    style: TextStyle(
+                        color: kMainColor, fontSize: 48, fontFamily: "Lobster"),
+                  ),
+                )),
+            Positioned(
+                top: 190,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
-                  ),
-
-
-
-                ),
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
-                  ),
-
-
-
-                ),
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
-                  ),
-
-
-
-                ),
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
-                  ),
-
-
-
-                ),
-                SizedBox(
-
-                  width: 900,
-                  child: ListTile(
-
-                    leading: Container(
-
-                      child: FloatingActionButton(
-                        backgroundColor: kMainColor,
-                        onPressed: (){},
-                        child: const Icon(Icons.add,size: 48,color: Color(0xff98694F),),
-
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
-
                     ),
-
-                    title:   const Text("Espresso ...................... \$3.00",style: TextStyle(fontSize: 24,fontFamily: "Lobster",color: kMainColor),),
-
-                    subtitle: const Text("Single shot Espresso",style: TextStyle(fontSize: 14,),),
-
-
-
-                  ),
-
-
-
-                ),
-
-
-
-              ],
-            )),
-
-
-
-
-
-
-
-
+                    SizedBox(
+                      width: 900,
+                      child: ListTile(
+                        leading: Container(
+                          child: FloatingActionButton(
+                            backgroundColor: kMainColor,
+                            onPressed: () {},
+                            child: const Icon(
+                              Icons.add,
+                              size: 48,
+                              color: Color(0xff98694F),
+                            ),
+                          ),
+                        ),
+                        title: const Text(
+                          "Espresso ...................... \$3.00",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontFamily: "Lobster",
+                              color: kMainColor),
+                        ),
+                        subtitle: const Text(
+                          "Single shot Espresso",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
           ],
-
         ),
       ),
-
     );
   }
 }
